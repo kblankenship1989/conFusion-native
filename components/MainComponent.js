462f70ce7727983cfbdb7e8357351e754dbd88d1
Kurtis Blankenship <BlankenshipKurtisA@JohnDeere.com>
2019-08-20 14:18:02 -0500

    Assignment 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
 
 import { Menu } from './MenuComponent';
 import { Dishdetail } from './DishdetailComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
 
 const MenuNavigator = createStackNavigator({
     Menu: { screen: Menu },
@@ -23,11 +26,69 @@ const MenuNavigator = createStackNavigator({
     }
 );
 
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home }
+}, {
+        navigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTitleStyle: {
+                color: "#fff"
+            },
+            headerTintColor: "#fff"
+        })
+    });
+
+const ContactNavigator = createStackNavigator({
+        Contact: { screen: Contact }
+    }, {
+            navigationOptions: ({ navigation }) => ({
+                headerStyle: {
+                    backgroundColor: "#512DA8"
+                },
+                headerTitleStyle: {
+                    color: "#fff"
+                },
+                headerTintColor: "#fff"
+            })
+        });
+
+const MainNavigator = createDrawerNavigator({
+    Home:
+    {
+        screen: HomeNavigator,
+        navigationOptions: {
+            title: 'Home',
+            drawerLabel: 'Home'
+        }
+    },
+    Menu:
+    {
+        screen: MenuNavigator,
+        navigationOptions: {
+            title: 'Menu',
+            drawerLabel: 'Menu'
+        },
+    },
+    Contact:
+    {
+        screen: ContactNavigator,
+        navigationOptions: {
+            title: 'Contact Us',
+            drawerLabel: 'Contact Us'
+        },
+    }
+}, {
+        drawerBackgroundColor: '#D1C4E9'
+    });
+
+
 class Main extends Component {
     render() {
         return (
             <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-                <MenuNavigator />
+                <MainNavigator />
             </View>
         );
     };
